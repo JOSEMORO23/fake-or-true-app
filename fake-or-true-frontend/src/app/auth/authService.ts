7ddd5fd5db9a1a4f7fd97f5914e9c6d8c18370ff
JOSEMORO23 <122384278+JOSEMORO23@users.noreply.github.com>
2025-07-23 18:16:25 -0500

    Agregar REACT_APP_API_URL desde docker-compose con ARG para producción

--- fake-or-true-frontend/src/app/auth/authService.ts
+++ fake-or-true-frontend/src/app/auth/authService.ts
@@ -1,7 +1,8 @@
 // src/app/auth/authService.ts
 import axios from "axios";
+const API = `${process.env.REACT_APP_API_URL}/auth`;
 
-const API = "http://localhost:8000/auth";
+//const API = "http://localhost:8000/auth";
 
 export const login = (email: string, password: string) =>
   axios.post(`${API}/login`, { email, password });
